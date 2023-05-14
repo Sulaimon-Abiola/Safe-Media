66758c31e17b14570e344075e30b025b09cfbbd9
Sulaimon <horyalekesulaimon@gmail.com>
2023-05-14 10:26:10 +0100

    Add the plugin headers as required by WordPress

---
+++ safe-media.php
@@ -0,0 +1,10 @@
+<?php
+/*
+  Plugin Name: Safe Media
+  Plugin URI: https://safemedia.com/
+  Description: Safe media assignment plugin.
+  Version: 1.0
+  Author: Oyaleke Sulaimon
+  Author URI: https://www.abiola.com/
+  Text Domain: safe-media
+*/
